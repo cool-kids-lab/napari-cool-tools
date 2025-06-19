2958b5a6febfcd45c9c646241970b9ae114f036a
John Jackson <jdjacks@sbcglobal.net>
2025-06-19 11:12:34 -0700

    comment out hotreload needs replacement in plugin

--- napari-cool-tools-io/src/napari_cool_tools_io/__init__.py
+++ napari-cool-tools-io/src/napari_cool_tools_io/__init__.py
@@ -233,14 +233,14 @@ def shortcut4(viewer):
         pass
 
 
-# specify list of modules that should be monitored
-list_of_modules = ["napari","napari_cool_tools_img_proc" ,"napari_cool_tools_io","napari_cool_tools_oct_preproc","napari_cool_tools_registration","napari_cool_tools_segmentation","napari_cool_tools_vol_proc"]
-
-widget = QtReloadWidget(list_of_modules)
-
-# add the widget to your application (or keep reference to it so it's not garbage collected)
-qt_viewer = napari.qt.QtViewer(viewer)
-qt_viewer.addWidget(widget)
-
-show_info(f"\n\nHOT RELOAD ACTIVATED!!\n\n")
-print(f"\n\nHOT RELOAD ACTIVATED!!\n\n")
+## specify list of modules that should be monitored
+#list_of_modules = ["napari","napari_cool_tools_img_proc" ,"napari_cool_tools_io","napari_cool_tools_oct_preproc","napari_cool_tools_registration","napari_cool_tools_segmentation","napari_cool_tools_vol_proc"]
+#
+#widget = QtReloadWidget(list_of_modules)
+#
+## add the widget to your application (or keep reference to it so it's not garbage collected)
+#qt_viewer = napari.qt.QtViewer(viewer)
+#qt_viewer.addWidget(widget)
+#
+#show_info(f"\n\nHOT RELOAD ACTIVATED!!\n\n")
+#print(f"\n\nHOT RELOAD ACTIVATED!!\n\n")
