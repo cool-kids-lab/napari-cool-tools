49180bb07af6133a186ad7bcc56b87e247d8bb6a
John Jackson <jdjacks@sbcglobal.net>
2025-07-15 08:45:00 -0700

    format equalization

--- napari-cool-tools-img-proc/src/napari_cool_tools_img_proc/_equalization.py
+++ napari-cool-tools-img-proc/src/napari_cool_tools_img_proc/_equalization.py
@@ -1,6 +1,7 @@
 """
 This module contains code for equalizing image values
 """
+
 from napari.layers import Image, Layer
 from napari.qt.threading import thread_worker
 from napari.utils.notifications import show_info
@@ -15,86 +16,127 @@ from napari_cool_tools_img_proc._equalization_funcs import (
 )
 
 
-def init_bscan_preproc_plugin(img:Image,num_std:int=16,min_intensity:float=0.0,max_intensity:float=1.0,dtype:DType=DType.NP_FLOAT):
-    '''
+def init_bscan_preproc_plugin(
+    img: Image,
+    num_std: int = 16,
+    min_intensity: float = 0.0,
+    max_intensity: float = 1.0,
+    dtype: DType = DType.NP_FLOAT64,
+):
+    """
     Args:
     Returns:
     Raises:
-    '''
-    init_bscan_preproc_thread(img=img,num_std=num_std,min_intensity=min_intensity,max_intensity=max_intensity,dtype=dtype)
+    """
+    init_bscan_preproc_thread(
+        img=img,
+        num_std=num_std,
+        min_intensity=min_intensity,
+        max_intensity=max_intensity,
+        dtype=dtype,
+    )
     return
 
 
-
 @thread_worker(connect={"returned": viewer.add_layer})
-def init_bscan_preproc_thread(img:Image,num_std:int=16,min_intensity:float=0.0,max_intensity:float=1.0,dtype:DType=DType.NP_FLOAT)->Layer:
-    '''
+def init_bscan_preproc_thread(
+    img: Image,
+    num_std: int = 16,
+    min_intensity: float = 0.0,
+    max_intensity: float = 1.0,
+    dtype: DType = DType.NP_FLOAT64,
+) -> Layer:
+    """
     Args:
     Returns:
     Raises:
-    '''
-    show_info('Init Bscan Preproc thread started')
+    """
+    show_info("Init Bscan Preproc thread started")
 
+    name = f"{img.name}_InitPreproc"
+    layer_type = "image"
+    add_kwargs = {"name": f"{name}"}
 
-    name = f'{img.name}_InitPreproc'
-    layer_type = 'image'
-    add_kwargs = {'name':f'{name}'}
+    proc_data = init_bscan_preproc(
+        img=img.data,
+        num_std=num_std,
+        min_intensity=min_intensity,
+        max_intensity=max_intensity,
+        dtype=dtype,
+    )
 
+    layer = Layer.create(proc_data, add_kwargs, layer_type)
 
-    proc_data = init_bscan_preproc(img=img.data,num_std=num_std,min_intensity=min_intensity,max_intensity=max_intensity,dtype=dtype)
-
-
-    layer = Layer.create(proc_data,add_kwargs,layer_type)
-
-
-    show_info('Thread Name thread completed')
+    show_info("Thread Name thread completed")
     return layer
 
-def background_removal_plugin(img:Image):
-    '''
+
+def background_removal_plugin(img: Image):
+    """
     Args:
     Returns:
     Raises:
-    '''
+    """
     background_removal_thread(img=img)
     return
 
 
 @thread_worker(connect={"returned": viewer.add_layer})
-def background_removal_thread(img:Image)->Image:
-    '''
+def background_removal_thread(img: Image) -> Image:
+    """
     Args:
     Returns:
     Raises:
-    '''
-    show_info('Background removal thread started')
+    """
+    show_info("Background removal thread started")
 
-
-    name = f'{img.name}_bg_corrected'
-    layer_type = 'image'
-    add_kwargs = {'name':f'{name}'}
-
+    name = f"{img.name}_bg_corrected"
+    layer_type = "image"
+    add_kwargs = {"name": f"{name}"}
 
     proc_data = background_removal_func(img=img.data)
 
+    layer = Layer.create(proc_data, add_kwargs, layer_type)
 
-    layer = Layer.create(proc_data,add_kwargs,layer_type)
-
-
-    show_info('Background removal thread completed')
+    show_info("Background removal thread completed")
     return layer
 
 
-def clahe(img:Image, kernel_size=None,clip_limit:float=0.01,nbins=256,norm_min=0,norm_max=1,pt_K:bool=True) -> Layer:
-    ''''''
-    clahe_thread(img=img,kernel_size=kernel_size,clip_limit=clip_limit,nbins=nbins,norm_min=norm_min,norm_max=norm_max,pt_K=pt_K)
+def clahe(
+    img: Image,
+    kernel_size=None,
+    clip_limit: float = 0.01,
+    nbins=256,
+    norm_min=0,
+    norm_max=1,
+    pt_K: bool = True,
+) -> Layer:
+    """"""
+    clahe_thread(
+        img=img,
+        kernel_size=kernel_size,
+        clip_limit=clip_limit,
+        nbins=nbins,
+        norm_min=norm_min,
+        norm_max=norm_max,
+        pt_K=pt_K,
+    )
 
     return
 
-@thread_worker(connect={"returned": viewer.add_layer},progress=True)
-def clahe_thread(img:Image, kernel_size=None,clip_limit:float=0.01,nbins=256,norm_min=0,norm_max=1,pt_K:bool=True) -> Layer:
-    ''''''
-    show_info('Autocontrast (CLAHE) thread has started')
+
+@thread_worker(connect={"returned": viewer.add_layer}, progress=True)
+def clahe_thread(
+    img: Image,
+    kernel_size=None,
+    clip_limit: float = 0.01,
+    nbins=256,
+    norm_min=0,
+    norm_max=1,
+    pt_K: bool = True,
+) -> Layer:
+    """"""
+    show_info("Autocontrast (CLAHE) thread has started")
 
     name = img.name
 
@@ -105,25 +147,40 @@ def clahe_thread(img:Image, kernel_size=None,clip_limit:float=0.01,nbins=256,nor
     layer_type = "image"
 
     if pt_K:
-        output = clahe_pt_func(data=img.data,kernel_size=kernel_size,clip_limit=clip_limit,nbins=nbins,norm_min=norm_min,norm_max=norm_max)
+        output = clahe_pt_func(
+            data=img.data,
+            kernel_size=kernel_size,
+            clip_limit=clip_limit,
+            nbins=nbins,
+            norm_min=norm_min,
+            norm_max=norm_max,
+        )
         torch.cuda.empty_cache()
         memory_stats()
     else:
-        output = clahe_func(data=img.data,kernel_size=kernel_size,clip_limit=clip_limit,nbins=nbins,norm_min=norm_min,norm_max=norm_max)
+        output = clahe_func(
+            data=img.data,
+            kernel_size=kernel_size,
+            clip_limit=clip_limit,
+            nbins=nbins,
+            norm_min=norm_min,
+            norm_max=norm_max,
+        )
 
-    layer = Layer.create(output,add_kwargs,layer_type)
+    layer = Layer.create(output, add_kwargs, layer_type)
 
-    show_info('Autocontrast (CLAHE) thread has completed')
+    show_info("Autocontrast (CLAHE) thread has completed")
     return layer
-    
-def match_histogram(target_histogram:Image,debug:bool=False):
+
+
+def match_histogram(target_histogram: Image, debug: bool = False):
     """"""
     from skimage.exposure import match_histograms
 
     target_data = target_histogram.data
     current_selection = list(viewer.layers.selection)
-    
+
     for layer in current_selection:
-        matched = match_histograms(layer.data,target_data,channel_axis=-1)
+        matched = match_histograms(layer.data, target_data, channel_axis=-1)
         layer.data[:] = matched[:]
     return layer
